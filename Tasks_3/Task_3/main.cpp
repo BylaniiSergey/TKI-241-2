c9f1ab44fbb8d028567a101a8bee75c416d3ddcb
BulaniiSergey <91825050+BylaniiSergey@users.noreply.github.com>
2025-04-07 22:47:27 +0300

    Исправил

--- Tasks_3/Task_3/main.cpp
+++ Tasks_3/Task_3/main.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
 #include <vector>
-#include <algorithm>
+#include <set>
 
 /**
 * @brief Точка входа в программу
@@ -10,21 +10,19 @@ int main()
 {
     std::vector<int> V = { 5, 2, 8, 2, 3, 5, 1, 8, 4 }; 
 
-    std::sort(V.begin(), V.end());
-
-    auto last = std::unique(V.begin(), V.end());
-    V.erase(last, V.end());
-
-    if (V.size() < 3) 
+    std::set<int> unique_elements(V.begin(), V.end());
+    if (unique_elements.size() < 3) 
     {
         std::cout << "Вектор содержит менее 3 уникальных элементов." << std::endl;
         return 0;
     }
 
-    V.erase(V.begin());
-    V.pop_back();
+    std::vector<int> unique_vec(unique_elements.begin(), unique_elements.end());
 
-    std::copy(V.rbegin(), V.rend(), std::ostream_iterator<int>(std::cout, " "));
+    unique_vec.erase(unique_vec.begin());
+    unique_vec.pop_back();
+
+    std::copy(unique_vec.rbegin(), unique_vec.rend(),std::ostream_iterator<int>(std::cout, " "));
     std::cout << std::endl;
 
     return 0;
