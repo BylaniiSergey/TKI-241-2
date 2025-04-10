d234915b573e45d3f9c84edcab1a3f13c7a60a91
BulaniiSergey <91825050+BylaniiSergey@users.noreply.github.com>
2025-04-10 17:29:24 +0300

    Задание 4

---
+++ Tasks_4/Task_4/point.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+
+struct Point
+{
+    int x;
+    int y;
+    std::string s;
+
+    /**
+    * @brief ќператор сравнени¤ дл¤ отношени¤ пор¤дка
+    */
+    bool operator<(const Point& other) const;
+
+    /**
+    * @brief ќператор ввода дл¤ структуры Point
+    */
+    friend std::istream& operator>>(std::istream& is, Point& p);
+
+    /**
+    * @brief ќператор вывода дл¤ структуры Point
+    */
+    friend std::ostream& operator<<(std::ostream& os, const Point& p);
+};
+
+/**
+* @brief ќбрабатывает файл с точками: читает, сортирует и перезаписывает
+* @param filename »м¤ файла дл¤ обработки
+*/
+void processFile(const std::string& filename);
